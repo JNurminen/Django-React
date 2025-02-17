fd5aebf56b19f29adaa32cda8205b26aa546f39a
Jani Nurminen2 <jani.nurminen2@student.careeria.fi>
2025-02-17 10:41:56 +0200

    App.jsx määritteljä. Notfound.jsx luotu ja Form.jsx luotu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import ProtectedRoute from "./components/ProtectedRoute"
 
+// Logout funktio poistaa local storagesta kaikki tiedot ja ohjaa käyttäjän kirjautumissivulle
+function Logout() {
+  localStorage.clear() // Poistetaan local storagesta kaikki tiedot
+  return <Navigate to="/login" /> // Ohjataan käyttäjä kirjautumissivulle
+}
+
+// Rekisteröitymisen jälkeen käyttäjä ohjataan kirjautumissivulle
+function RegisterAndLogout() {
+  localStorage.clear() // Poistetaan local storagesta kaikki tiedot
+  return <Register /> // Ohjataan käyttäjä rekisteröitymissivulle
+}
+
+// Sovelluksen pääkomponentti
 function App() {
   return (
-    <>
-
-    </>
+    <BrowserRouter> // Reititys 
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute> // Suojattu reitti, joka ohjaa käyttäjän kirjautumissivulle, jos käyttäjä ei ole kirjautunut
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} /> // Kirjautumissivu
+        <Route path="/logout" element={<Logout />} /> // Uloskirjautumissivu
+        <Route path="/register" element={<RegisterAndLogout />} /> // Rekisteröitymissivu
+        <Route path="*" element={<NotFound />}></Route> // 404-sivu
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/components/Form.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import api from "../api";
+import { useNavigate } from "react-router-dom";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+
+// Form komponentti, joka ottaa propsinaan reitin ja metodin
+function Form({ route, method }) {
+    const [username, setUsername] = useState(""); // Käyttäjänimi
+    const [password, setPassword] = useState(""); // Salasana
+    const [loading, setLoading] = useState(false); 
+    const navigate = useNavigate(); // Reittien ohjaus
+
+    const name = method === "login" ? "Login" : "Register"; 
+
+    const handleSubmit = async (e) => { // Lomakkeen lähetyksen käsittely
+        setLoading(true);
+        e.preventDefault();
+
+        // Yritetään lähettää käyttäjänimi ja salasana backendille
+        try {
+            const res = await api.post(route, { username, password }) // Lähetetään käyttäjänimi ja salasana backendille
+            if (method === "login") { // Jos metodi on login, tallennetaan käyttäjän tokenit local storageen ja ohjataan käyttäjä etusivulle
+                localStorage.setItem(ACCESS_TOKEN, res.data.access); 
+                localStorage.setItem(REFRESH_TOKEN, res.data.refresh); 
+                navigate("/") 
+            } else { // Jos metodi on register, ohjataan käyttäjä kirjautumissivulle
+                navigate("/login") 
+            }
+        } catch (error) { // Jos lähetyksessä tapahtuu virhe, näytetään virheilmoitus
+            alert(error)
+        } finally {
+            setLoading(false)
+        }
+    };
+
+    // Lomake, jossa käyttäjä voi syöttää käyttäjänimensä ja salasanansa
+    return (
+        <form onSubmit={handleSubmit} className="form-container">
+            <h1>{name}</h1>
+            <input
+                className="form-input"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+            />
+            <input
+                className="form-input"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+            />
+            {loading && <LoadingIndicator />}
+            <button className="form-button" type="submit">
+                {name}
+            </button>
+        </form>
+    );
+}
+
+export default Form
